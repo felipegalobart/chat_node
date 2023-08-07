ec59c6658805c2abe476a91c782f31b9406ebf81
Felipe Galobart Jora <felipe@mitsuwa.com.br>
2023-08-07 19:41:08 -0300

    estabelecendo conexao com o servidor

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(80);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 console.log('Servidor Rodando na porta 80')
+
+io.on('connection', (socket) => {
+    console.log("Conexão detectada....");
+});
 
